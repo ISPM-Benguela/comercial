2cac79ba33d1b618f1f257b60d24ad193a7c441b
tandavala <jose.tandavala@gmail.com>
2018-12-04 18:14:52 +0100

    adding carousel to the main product page

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -7,15 +7,35 @@
  <link rel="stylesheet" href="css/now.css" />
  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
  <link rel="stylesheet" href="css/backoffice/produto.css">
+ <link rel="stylesheet" href="css/backoffice/owl.carousel.min.css">
+ <link rel="stylesheet" href="css/backoffice/owl.theme.default.css">
 @stop
 @section('principal')
   <div class="produtos">
-     <h3><i class="fa fa-plus"></i> Produtos desponivel</h3>
+     <h3><i class="fa fa-shopping-basket"></i> Produtos em destaque</h3>
          <br />
         <a href="{{ route('produtos.create')}}" class="btn btn-primary" ><i class="fa fa-plus"></i> Cadastrar produto</a>
-     <div class="produtos-cabeca">
-     
-     </div>
+     <div class="container produtos-cabeca">
+       <div class="row">
+         <div class="owl-carousel owl-them">
+           <div class="item thumbnail">
+             <img src="images/product-01.jpg" />
+           </div><!-- ./ item -->
+           <div class="item thumbnail">
+           <img src="images/product-02.jpg" />
+           </div><!-- ./ item -->
+           <div class="item thumbnail">
+           <img src="images/product-03.jpg" />
+           </div><!-- ./ item -->
+           <div class="item thumbnail">
+           <img src="images/product-04.jpg" />
+           </div><!-- ./ item -->
+           <div class="item thumbnail">
+           <img src="images/product-05.jpg" />
+           </div><!-- ./ item -->
+         </div>
+       </div>
+     </div><!--./ produtos-cabecas -->
      <div class="produtos-corpo">
 
      </div>
@@ -26,6 +46,7 @@
 
 @stop
 @section('footer')
- <script src="js/now.js"></script>
+ <script src="js/backoffice/owl.carousel.js"></script>
+ <script src="js/backoffice/app.js"></script>
 @stop
 
