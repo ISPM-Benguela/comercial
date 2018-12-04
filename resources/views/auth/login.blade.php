81676009016f4d55429172f22cedaf8aec96cf3a
tandavala <jose.tandavala@gmail.com>
2018-12-04 16:54:18 +0100

    refactoring the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,65 @@
 @extends('layouts.autente')
 
+@section('head')
+ <link rel="stylesheet" href="{{asset('css/login.css')}}">
+@stop
+
 @section('content')
-<div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Login</div>
-    
-                    <div class="panel-body">
-                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-    
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-    
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-    
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-    
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
-    
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-    
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-    
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                        </label>
+    <div class="container">
+        <div class="principal">
+            <img src="images/user.jpg"   />
+
+            <h3>Entrar no sistema</h3>
+
+            <form action="{{ route('login') }}" method="post" >
+                {{ csrf_field() }}
+
+               {{ csrf_field() }}
+                    <div class="pb-block" id="divLoginContro" style="padding: 10px;">   
+                        <div class="row main" >
+                                <div class="form-group col-md-12 {{ $errors->has('email') ? ' is-invalid' : '' }}">
+                                            <input
+                                                    id="email"
+                                                    type="text"
+                                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                                    name="email"
+                                                    value="{{ old('email') }}"
+                                                    placeholder="Informar o email"
+                                            >
+            
+                                            @if ($errors->has('email'))
+                                                <div class="invalid-feedback">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </div>
+                                            @endif
                                     </div>
+                                    <div class="form-group col-md-12">
+                                            <input
+                                                    id="password"
+                                                    type="password"
+                                                    class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                                    name="password"
+                                                    value="{{ old('password') }}"
+                                                    placeholder="informar a senha"
+                                            >
+            
+                                            @if ($errors->has('password'))
+                                                <div class="invalid-feedback">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </div>
+                                            @endif
+                                    </div>
+                            <div class="form-group col-md-12">
+                                <button type="submit" class="btn btn-primary btn-block">Iniciar Sess&atilde;o</button>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <div class="col-md-12">
+                                    <a href="{{ route('home')}}" class="btn btn-link btn-block">P&aacute;gina Inicial</a>
                                 </div>
                             </div>
-    
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Login
-                                    </button>
-    
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        Forgot Your Password?
-                                    </a>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    @stop
+            </form>
+        </div><!-- ./ principal -->    
+    </div><!-- ./ container -->
+@stop
--- resources/views/layouts/autente.blade.php
+++ resources/views/layouts/autente.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo','iShopping')</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 
     <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    @yield('head')
 </head>
 <body>
     @yield('content')
