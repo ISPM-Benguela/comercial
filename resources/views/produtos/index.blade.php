d36b4b4594ed999a517f19e91de07a9b469ea8c5
tandavala <jose.tandavala@gmail.com>
2018-12-08 04:56:25 +0100

    adicionado as imagens proprias do negocio

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -5,10 +5,11 @@
 @stop
 @section('head')
  <link rel="stylesheet" href="css/now.css" />
- <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
- <link rel="stylesheet" href="css/backoffice/produto.css">
+ 
+
  <link rel="stylesheet" href="css/backoffice/owl.carousel.min.css">
  <link rel="stylesheet" href="css/backoffice/owl.theme.default.css">
+ <link rel="stylesheet" href="css/backoffice/produto.css">
 @stop
 @section('principal')
   <div class="produtos">
@@ -19,65 +20,65 @@
        <div class="row">
          <div class="owl-carousel owl-them">
            <div class="item thumbnail">
-             <img src="images/product-15.jpg" />
+             <img src="images/produtos/product01.png" width="600" height="600" />
              <div class="caption">
-                <h3>Relogio Gucci</h3>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+                <h3> NoteBook PC 2000</h3>
+                <p class="catalogo">123.000 Kz <small>Estoque: 33</small></p>
 
-                <p>Este e um bom produto que vale a penas
-                 dar um golpede vista nele.</p>
+                <p>Este computador é ideial para programadores
+                  tem 4 GB de RAM 1 Tera de espaço.</p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                    <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="item thumbnail">
-              <img src="images/product-02.jpg" />
+              <img src="images/produtos/product02.png" width="600" height="600" />
               <div class="caption">
-                <h3>Nome do produto</h3>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+                <h3>Auscutador Bluetooth</h3>
+                <p class="catalogo">23.000 Kz <small>Estoque: 33</small></p>
 
-                <p>Este e um bom produto que vale a penas
-                 dar um golpede vista nele.</p>
+                <p>Este aucutador é ideial para ouvir boa musica
+                    em qualquer lugar suporta bluetooth.</p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                   <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="item thumbnail">
-             <img src="images/product-06.jpg" />
+             <img  src="images/produtos/product10.jpeg" width="600" height="600"  />
              <div class="caption">
-                <h3>Nome do produto</h3>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+                <h3>iPhone 8 Plus</h3>
+                <p class="catalogo">123.000 Kz <small>Estoque: 33</small></p>
 
-                <p>Este e um bom produto que vale a penas
-                 dar um golpede vista nele.</p>
+                <p>Este e um dos recentes produto da apple com grandes 
+                  capacidade ele tem de espaço 64 BG.</p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a> 
                  <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="item thumbnail">
-             <img src="images/product-04.jpg" />
+              <img  src="images/produtos/product11.jpeg" width="600" height="600"  />
               <div class="caption">
-                <h3>Nome do produto</h3>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+                <h3>Mix Table</h3>
+                <p class="catalogo">230.000 Kz <small>Estoque: 33</small></p>
 
-                <p>Este e um bom produto que vale a penas
-                 dar um golpede vista nele.</p>
+                <p>Esta é uma mix table ideial para dj profissional
+                  este producto foi feito pensando em alta performace..</p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                   <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="item thumbnail">
-           <img src="images/product-05.jpg" />
+              <img  src="images/produtos/product09.png" width="600" height="600"
            <div class="caption">
-                <h3>Nome do produto</h3>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+                <h3>Camera Digital</h3>
+                <p class="catalogo">83.000 Kz <small>Estoque: 33</small></p>
 
-                <p>Este e um bom produto que vale a penas
-                 dar um golpede vista nele.</p>
+                <p>Este produto pode ser usado por todos paricularmente profissionais
+                  da area o produto foi feito pensando no facil uso e qualidade.</p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                   <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
@@ -86,45 +87,45 @@
          </div>
        </div>
      </div><!--./ produtos-cabecas -->
-     <div class="produtos-corpo">
-       <h3><i class="fa fa-barcode"></i> Produtos desponiveis</h3>
+     <div class="produtos-corpo" style="background: #ccc; padding: 20px;">
+       <h3 style="padding: 10px; text-transform: uppercase; padding-bottom: 15px; color: #888; font-weight: bolder;"><i class="fa fa-barcode"></i> Produtos desponiveis</h3>
        <div class="row">
          
            <div class="col-md-3 thumbnail">
-              <img src="images/product-02.jpg" />
+              <img src="images/produtos/product07.png" width="600" height="600" />
               <div class="caption">
-              <p>Nome do produto</p>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+              <p>Samsung Gallaxy</p>
+                <p class="catalogo">100.000 Kz <small>Estoque: 33</small></p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                   <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="col-md-3 thumbnail">
-             <img src="images/product-06.jpg" />
+              <img src="images/produtos/product04.png" width="600" height="600" />
              <div class="caption">
-                <p>Nome do produto</p>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+                <p>Tablet Samsung</p>
+                <p class="catalogo">93.000 Kz <small>Estoque: 33</small></p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a> 
                  <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="col-md-3 thumbnail">
-             <img src="images/product-04.jpg" />
+              <img src="images/produtos/product08.png" width="600" height="600" />
               <div class="caption">
-              <p>Nome do produto</p>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+              <p>LapTop Dell</p>
+                <p class="catalogo">73.000 Kz <small>Estoque: 33</small></p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                   <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
              </div><!-- ./ caption -->
            </div><!-- ./ item -->
            <div class="col-md-3 thumbnail">
-             <img src="images/product-04.jpg" />
+              <img src="images/produtos/product03.png" width="600" height="600" />
               <div class="caption">
-              <p>Nome do produto</p>
-                <p class="catalogo">23.00 Kz <small>Estoque: 33</small></p>
+              <p>NoteBook Samsung</p>
+                <p class="catalogo">230.000 Kz <small>Estoque: 33</small></p>
 
                  <p><a href="#" class="btn btn-primary" role="button"><i class="fa fa-pencil"></i> Editar</a>
                   <a href="#" class="btn btn-danger" role="button"><i class="fa fa-trash"></i> Eliminar</a></p>
